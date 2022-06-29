1ff1bef51fa237d75d43fa44b6bb4e5d1b09587f
Steve McHenry <smchenry@cirarad.net>
2022-06-29 18:44:03 -0500

    Initial commit

---
+++ sql-scratch-queries/Get Distinct Violation Code and Text.sql
@@ -0,0 +1,29 @@
+-- Note: Assumes the raw dataset has been loaded into a table named "FoodInspections"
+WITH SampleInspections AS
+(
+	SELECT *
+	FROM FoodInspections
+)
+
+SELECT DISTINCT ViolationID
+	,ViolationText
+FROM
+(
+	SELECT CAST(SUBSTRING(RawViolationText, 0, ViolationPosition) AS INT) AS ViolationID
+		,SUBSTRING(RawViolationText, (ViolationPosition + 2), (ViolationCommentPosition - (ViolationPosition + 2))) AS ViolationText
+		,SUBSTRING(RawViolationText, ViolationCommentPosition + 12, LEN(RawViolationText)) AS ViolationComment
+		,RawViolationText
+	FROM
+	(
+		SELECT PATINDEX('% - Comments:%', RawViolationText) AS ViolationCommentPosition
+			,PATINDEX('%.%', RawViolationText) AS ViolationPosition
+			,RawViolationText
+		FROM
+		(
+			SELECT TRIM(value) AS RawViolationText
+			FROM SampleInspections
+				CROSS APPLY STRING_SPLIT(Violations, '|')
+		) AS RawViolation
+	) AS InspectedRawViolation
+) AS ExtractedViolation
+ORDER BY ViolationID ASC;
