e9db1e859eec441a18f0e4fd3bb857d93f28149a
Steve McHenry <smchenry@cirarad.net>
2022-07-28 14:11:29 -0500

    Implement additional ICV demonstration queries

--- sql/Delta D Analysis.sql
+++ sql/Delta D Analysis.sql
@@ -4,64 +4,185 @@ USE CDPH;
 -- as case-sensitive, accent-sensitive to avoid underreporting by databases
 -- or columns configured with case-insensitive collations
 
--- DBA Name field number of merged values
+-- *******************
+-- Inspection ID field
+-- *******************
+
+-- Demonstrate that there are no duplicate Inspection IDs in the raw source data
+WITH DataRaw_Inspection_ID AS
+(
+	SELECT DR.Inspection_ID
+	FROM DataRaw AS DR
+	GROUP BY DR.Inspection_ID
+	HAVING COUNT(DR.Inspection_ID) > 1
+)
+
+SELECT COUNT(DataRaw_Inspection_ID.Inspection_ID) AS Inspection_ID_DataRaw_Duplicate
+FROM DataRaw_Inspection_ID;
+
+-- Demonstrate that there are no duplicate Inspection IDs in the cleaned data
+WITH DataOpenRefine_Inspection_ID AS
+(
+	SELECT DOR.Inspection_ID
+	FROM DataOpenRefine AS DOR
+	GROUP BY DOR.Inspection_ID
+	HAVING COUNT(DOR.Inspection_ID) > 1
+)
+
+SELECT COUNT(DataOpenRefine_Inspection_ID.Inspection_ID) AS Inspection_ID_DataOpenRefine_Duplicate
+FROM DataOpenRefine_Inspection_ID;
+
+GO
+
+-- **************
+-- DBA Name field
+-- **************
+
+-- Calculate the number of DBA Name values that were merged as inconsistent duplicates
 WITH DataRaw_DBA_Name AS
 (
-	SELECT DISTINCT DR.DBA_Name COLLATE Latin1_General_CS_AS AS [Name]
+	SELECT DISTINCT DR.DBA_Name COLLATE Latin1_General_CS_AS AS DBA_Name
 	FROM DataRaw AS DR
 	GROUP BY DR.DBA_Name COLLATE Latin1_General_CS_AS
 )
 
 ,DataOpenRefine_DBA_Name AS
 (
-	SELECT DISTINCT DOR.DBA_Name AS [Name]
+	SELECT DISTINCT DOR.DBA_Name AS DBA_Name
 	FROM DataOpenRefine AS DOR
 	GROUP BY DOR.DBA_Name
 )
 
-SELECT (SELECT COUNT(*) FROM DataRaw_DBA_Name) - (SELECT COUNT(*) FROM DataOpenRefine_DBA_Name) AS MergedCount_DBA_Name;
+SELECT (SELECT COUNT(*) FROM DataRaw_DBA_Name) - (SELECT COUNT(*) FROM DataOpenRefine_DBA_Name) AS MergedValueCount_DBA_Name;
+
+-- Calculate the number of DBA Name elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.DBA_Name) AS MergedElementCount_DBA_Name
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.DBA_Name COLLATE Latin1_General_CS_AS <> DR.DBA_Name COLLATE Latin1_General_CS_AS
 
 GO
 
--- AKA Name field number of merged values
+-- **************
+-- AKA Name field
+-- **************
+
+-- Calculate the number of AKA Name values that were merged as inconsistent duplicates
 WITH DataRaw_AKA_Name AS
 (
-	SELECT DISTINCT DR.AKA_Name COLLATE Latin1_General_CS_AS AS [Name]
+	SELECT DISTINCT DR.AKA_Name COLLATE Latin1_General_CS_AS AS AKA_Name
 	FROM DataRaw AS DR
 	GROUP BY DR.AKA_Name COLLATE Latin1_General_CS_AS
 )
 
 ,DataOpenRefine_AKA_Name AS
 (
-	SELECT DISTINCT DOR.AKA_Name AS [Name]
+	SELECT DISTINCT DOR.AKA_Name AS AKA_Name
 	FROM DataOpenRefine AS DOR
 	GROUP BY DOR.AKA_Name
 )
 
-SELECT (SELECT COUNT(*) FROM DataRaw_AKA_Name) - (SELECT COUNT(*) FROM DataOpenRefine_AKA_Name) AS MergedCount_AKA_Name;
+SELECT (SELECT COUNT(*) FROM DataRaw_AKA_Name) - (SELECT COUNT(*) FROM DataOpenRefine_AKA_Name) AS MergedValueCount_AKA_Name;
+
+-- Calculate the number of AKA Name elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.AKA_Name) AS MergedElementCount_AKA_Name
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.AKA_Name COLLATE Latin1_General_CS_AS <> DR.AKA_Name COLLATE Latin1_General_CS_AS
 
 GO
 
--- Address field number of merged values
+-- *************
+-- License field
+-- *************
+
+-- Demonstrate that 15 license elements (cells) were empty (NULL) in the raw source data
+SELECT COUNT(*) AS License_DataRaw_NULL
+FROM DataRaw AS DR
+WHERE DR.License IS NULL
+
+-- Demonstrate that all empty (NULL) license elsments are resolved in the cleaned data
+SELECT COUNT(*) AS License_DataOpenRefine_NULL
+FROM DataOpenRefine AS DOR
+WHERE DOR.License IS NULL
+
+GO
+
+-- *******************************************
+-- Facility Type field (optional/supplemental)
+-- *******************************************
+
+-- Calculate the number of Facility Type values that were optionally merged as inconsistent duplicates
+WITH DataRaw_Facility_Type AS
+(
+	SELECT DISTINCT DR.Facility_Type COLLATE Latin1_General_CS_AS AS Facility_Type
+	FROM DataRaw AS DR
+	GROUP BY DR.Facility_Type COLLATE Latin1_General_CS_AS
+)
+
+,DataOpenRefine_Facility_Type AS
+(
+	SELECT DISTINCT DOR.Facility_Type AS Facility_Type
+	FROM DataOpenRefine AS DOR
+	GROUP BY DOR.Facility_Type
+)
+
+SELECT (SELECT COUNT(*) FROM DataRaw_Facility_Type) - (SELECT COUNT(*) FROM DataOpenRefine_Facility_Type) AS MergedCount_FacilityType;
+
+-- Calculate the number of Facility Type elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.Facility_Type) AS MergedElementCount_Facility_Type
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.Facility_Type COLLATE Latin1_General_CS_AS <> DR.Facility_Type COLLATE Latin1_General_CS_AS
+
+GO
+
+-- *************
+-- Address field
+-- *************
+
+-- Calculate the number of address values that were merged as inconsistent duplicates
 WITH DataRaw_Address AS
 (
 	SELECT DISTINCT DR.[Address] COLLATE Latin1_General_CS_AS AS [Address]
 	FROM DataRaw AS DR
-	GROUP BY DR.Address COLLATE Latin1_General_CS_AS
+	GROUP BY DR.[Address] COLLATE Latin1_General_CS_AS
 )
 
 ,DataOpenRefine_Address AS
 (
 	SELECT DISTINCT DOR.[Address] AS [Address]
 	FROM DataOpenRefine AS DOR
-	GROUP BY DOR.Address
+	GROUP BY DOR.[Address]
 )
 
 SELECT (SELECT COUNT(*) FROM DataRaw_Address) - (SELECT COUNT(*) FROM DataOpenRefine_Address) AS MergedCount_Address;
 
+-- Calculate the number of Address elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.[Address]) AS MergedElementCount_Address
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.[Address] COLLATE Latin1_General_CS_AS <> DR.[Address] COLLATE Latin1_General_CS_AS
+	OR DATALENGTH(DOR.[Address]) <> DATALENGTH(DR.[Address])
+	OR DR.[Address] IS NULL;
+
 GO
 
--- City field number of merged values
+-- **********
+-- City field
+-- **********
+
+-- Number of NULL City elements in the original raw data
+SELECT COUNT(*) AS City_DataRaw_NULL
+FROM DataRaw AS DR
+WHERE DR.City IS NULL;
+
+-- Number of NULL City elements in the cleaned data
+SELECT COUNT(*) AS City_DataCleaned_NULL
+FROM DataOpenRefine AS DOR
+WHERE DOR.City IS NULL;
+
+-- Calculate the number of city values that were merged as inconsistent duplicates
 WITH DataRaw_City AS
 (
 	SELECT DISTINCT DR.City COLLATE Latin1_General_CS_AS AS City
@@ -76,24 +197,147 @@ WITH DataRaw_City AS
 	GROUP BY DOR.City
 )
 
-SELECT (SELECT COUNT(*) FROM DataRaw_City) - (SELECT COUNT(*) FROM DataOpenRefine_City) AS MergedCount_City;
+SELECT (SELECT COUNT(*) FROM DataRaw_City) - (SELECT COUNT(*) FROM DataOpenRefine_City) AS MergedValueCount_City;
+
+-- Calculate the number of City elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.City) AS MergedElementCount_City
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.City COLLATE Latin1_General_CS_AS <> DR.City COLLATE Latin1_General_CS_AS
+	OR DR.City IS NULL;
 
 GO
 
--- Zip field number of NULL values resolved
-SELECT COUNT(*) AS ResolvedCount_Zip
+-- ***********
+-- State field
+-- ***********
+
+-- Demonstrate that 8 state elements were empty (NULL) in the raw source data
+SELECT COUNT(*) AS State_DataRaw_NULL
 FROM DataRaw AS DR
-WHERE DR.Zip IS NULL;
+WHERE DR.[State] IS NULL
+	OR TRIM(DR.[State]) = '';
+
+-- Demonstrate that no state elements were empty (NULL) in the cleaned data
+SELECT COUNT(*) AS State_DataOpenRefine_NULL
+FROM DataOpenRefine AS DOR
+WHERE DOR.[State] IS NULL;
 
 GO
 
--- Location field number of NULL values resolved
-SELECT COUNT(*) AS ResolvedCount_Location
+-- *********
+-- Zip field
+-- *********
+
+-- Number of NULL Zip elements in the original dataset
+SELECT COUNT(*) AS Zip_DataRaw_NULL
 FROM DataRaw AS DR
-WHERE DR.[Location] IS NULL;
+WHERE DR.Zip IS NULL
+	OR TRIM(DR.Zip) = '';
+
+-- Number of NULL Zip elements in the cleaned dataset
+SELECT COUNT(*) AS Zip_DataCleaned_NULL
+FROM DataOpenRefine AS DOR
+WHERE DOR.Zip IS NULL;
+
+-- Calculate the number of Zip elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.Zip) AS MergedElementCount_Zip
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.Zip COLLATE Latin1_General_CS_AS <> DR.Zip COLLATE Latin1_General_CS_AS
+	OR DR.Zip IS NULL;
 
 GO
 
--- Violation field number of violations
-SELECT COUNT(*) AS Count_Violations
+-- *********************************************
+-- Inspection Type field (optional/supplemental)
+-- *********************************************
+
+-- Calculate the number of Inspection Type values that were optionally merged as inconsistent duplicates
+WITH DataRaw_Inspection_Type AS
+(
+	SELECT DISTINCT DR.Inspection_Type COLLATE Latin1_General_CS_AS AS Inspection_Type
+	FROM DataRaw AS DR
+	GROUP BY DR.Inspection_Type COLLATE Latin1_General_CS_AS
+)
+
+,DataOpenRefine_Inspection_Type AS
+(
+	SELECT DISTINCT DOR.Inspection_Type AS Inspection_Type
+	FROM DataOpenRefine AS DOR
+	GROUP BY DOR.Inspection_Type
+)
+
+SELECT (SELECT COUNT(*) FROM DataRaw_Inspection_Type) - (SELECT COUNT(*) FROM DataOpenRefine_Inspection_Type) AS MergedCount_Inspection_Type;
+
+-- Calculate the number of Inspection Type elements (cells) that were merged as inconsistent duplicates
+SELECT COUNT(DOR.Inspection_Type) AS MergedElementCount_InspectionType
+FROM DataOpenRefine AS DOR
+	INNER JOIN DataRaw AS DR ON DOR.Inspection_ID = DR.Inspection_ID
+WHERE DOR.Inspection_Type COLLATE Latin1_General_CS_AS <> DR.Inspection_Type COLLATE Latin1_General_CS_AS
+
+GO
+
+-- *********************************************
+-- Violations field
+-- *********************************************
+
+-- Demonstrate that 568,654 violation instances exist in the raw source data
+WITH DataRaw_InspectionViolation AS
+(
+	SELECT Inspection_ID
+		,ViolationID
+		,ViolationText
+		,CASE WHEN LEN(ViolationComment) > 0 THEN ViolationComment ELSE NULL END AS ViolationComment
+	FROM
+	(
+		SELECT Inspection_ID
+			,CAST(SUBSTRING(RawViolationText, 0, ViolationPosition) AS INT) AS ViolationID
+			,SUBSTRING(RawViolationText, (ViolationPosition + 2), (ViolationCommentPosition - (ViolationPosition + 2))) AS ViolationText
+			,TRIM(SUBSTRING(RawViolationText, ViolationCommentPosition + 12, LEN(RawViolationText))) AS ViolationComment
+			,RawViolationText
+		FROM
+		(
+			SELECT Inspection_ID
+				,PATINDEX('% - Comments:%', RawViolationText) AS ViolationCommentPosition
+				,PATINDEX('%.%', RawViolationText) AS ViolationPosition
+				,RawViolationText
+			FROM
+			(
+				SELECT Inspection_ID
+					,TRIM(value) AS RawViolationText
+				FROM DataRaw
+					CROSS APPLY STRING_SPLIT(Violations, '|')
+			) AS RawViolation
+		) AS InspectedRawViolation
+	) AS ExtractedViolation
+)
+
+SELECT COUNT(DataRaw_InspectionViolation.ViolationID) AS Violations_DataRaw_Count
+FROM DataRaw_InspectionViolation;
+
+-- Demonstrate that all 568,654 violations are properly extracted during the Stage 3 normalization process
+SELECT COUNT(*) AS Violations_DataOpenRefine_Count
 FROM InspectionViolation;
+
+GO
+
+-- ************************************
+-- Longitude, Latitude, Location fields
+-- ************************************
+
+-- Demonstrate that 544 records contained empty (NULL) location elements in the raw source data
+SELECT COUNT(*) AS Location_DataRaw_NULL
+FROM DataRaw AS DR
+WHERE DR.[Location] IS NULL
+	OR DR.Longitude IS NULL
+	OR DR.Latitude IS NULL;
+
+-- Demonstrate that no records contained empty (NULL) location elements in the cleaned data
+SELECT COUNT(*) AS Location_DataCleaned_NULL
+FROM Business
+WHERE Business.[Location] IS NULL
+	OR Business.Longitude IS NULL
+	OR Business.Latitude IS NULL;
+
+GO
